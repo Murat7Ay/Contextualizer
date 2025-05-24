4cd3ebc5772ded4f448acbfcc8012576c0d23616
Murat Ay <murataybtp@gmail.com>
2025-05-24 22:20:54 +0300

    feat(plugin): add ShowWindow functionality to plugin interface and implementation

---
+++ Contextualizer.Plugins/ShowWindow.cs
@@ -0,0 +1,27 @@
+﻿using Contextualizer.PluginContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contextualizer.Plugins
+{
+    public class ShowWindow
+    {
+        private IPluginServiceProvider pluginServiceProvider;
+        public string Name => "show_window";
+
+        public void Action(ConfigAction action, ContextWrapper context)
+        {
+            context[ContextKey._body] = context[action.Key];
+
+            pluginServiceProvider.GetService<IUserInteractionService>().ShowWindow(context._handlerConfig.ScreenId, context._handlerConfig.Title, context, new());
+        }
+
+        public void Initialize(IPluginServiceProvider serviceProvider)
+        {
+            pluginServiceProvider = serviceProvider;
+        }
+    }
+}
